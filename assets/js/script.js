1627c372c127aa30cc2ffeb85d810170399fbefe
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-08 21:45:39 -0400

    finished 4.2.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,43 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // creating handler to, create new li, assign class for styling and text content, append to list
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     // default behavior of form submit is to refresh page, must prevent this from occuring
     event.preventDefault();
+    // using [ ] in a selector means you're trying to select an HTMl element by an attribute
+    // ex in this case selecting the <input> element with the name="task-name" attribute
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // form validation to prevent empty inputs
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+    // package data as object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // send as argument to createTaskEl
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = 'task-item';
-    listItemEl.textContent = "This is a new task.";
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    // add entire div to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
 // callback function: on form submit create a task
 // targetting entire form, so you do not want to have event listener set to "click"
 // or else everytime you click anywhere on the form, callback function will occur
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
