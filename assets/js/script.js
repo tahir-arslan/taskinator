17351848ca3a6eb2edc50a1d0ff1abd5bae8229b
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-08 21:12:34 -0400

    finished 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var formEl = document.querySelector("#task-form");
 var buttonEl = document.querySelector("#save-task");
 // if element will be referenced more than once, best to assign it to a variable
 // if not and query for element is needed, it will search DOM everytime and will
@@ -5,12 +6,16 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // creating handler to, create new li, assign class for styling and text content, append to list
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    // default behavior of form submit is to refresh page, must prevent this from occuring
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = 'task-item';
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// callback function: on button click create a task
-buttonEl.addEventListener("click", createTaskHandler);
+// callback function: on form submit create a task
+// targetting entire form, so you do not want to have event listener set to "click"
+// or else everytime you click anywhere on the form, callback function will occur
+formEl.addEventListener("submit", createTaskHandler);
